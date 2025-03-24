dc59fa32412b2291fafe975491aa4f133feeae07
Baptiste Pillot <baptiste@pillot.fr>
2025-03-24 14:29:40 +0100

    Replace default exports with named exports

--- contained-auto-width.ts
+++ contained-auto-width.ts
@@ -1,5 +1,5 @@
 
-export default function containedAutoWidth(container: HTMLElement)
+export function containedAutoWidth(container: HTMLElement)
 {
 	if (['OL', 'UL'].includes(container.nodeName)) {
 		for (
